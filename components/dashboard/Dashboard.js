6b66be55200f58ace698f298a5e719ed73a10972
mike15395 <mikhildesai@gmail.com>
2022-03-06 00:09:48 +0530

    navigate functionality added

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
 import './Dashboard.css'
+import {useNavigate} from 'react-router-dom'
 
 function Dashboard() {
+
+    const navigate = useNavigate();
+
+    const getData = () => {
+
+        // some pieces of code here then execute navigate
+        navigate('/');
+    }
 
     const [text, setText] = useState("");
     const [array, setArray] = useState([]);
@@ -22,7 +31,8 @@ function Dashboard() {
   return (
       <div >
 
-          <h1>Hello you have been routed to dashboard page</h1>
+          <h1>Hello you have been routed to dashboard page</h1><br/>
+          <button onClick={getData}>Navigate to home</button>
           <textarea placeholder='write some text here' id="textarea" value={text} onChange={(e) => setText(e.target.value)}></textarea><br />
           <button id='add-button'onClick={addText}>Add</button>
 
